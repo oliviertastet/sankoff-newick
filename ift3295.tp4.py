a004ea286f07f3990d5d975e56619c4b92a18688
oliviertastet <olivier.tastet@umontreal.ca>
2014-11-20 03:20:35 -0500

    Classified node

--- ift3295.tp4.py
+++ ift3295.tp4.py
@@ -2,71 +2,8 @@
 import math
 import argparse
 import sys
+from node import Node
 
-# La classe Node instancie les noeuds d'un arbre binaire. Il est binaire par définition car un noeud ne peux avoir que 2 enfants 
-# La méthode add_child permet d'ajouter un enfant à un noeud de manière à ce que l'arbre soit balancé. Si on demande l'ajout de 3 
-# enfants, l'assignation de l'enfant ne se fait pas et un message d'erreur est retourné. La classe contient également plusieurs 
-# méthodes d'accession et de fixation
-class Node():
-    def __init__(self,label):
-        self.right = None
-        self.left = None 
-        self.parent = None
-        self.label = label
-        self.sequence = None
-        self.score= 0
-        self.scores = [float('inf')]*23
-
-    def add_child(self, node):
-        if self.left == None:
-            self.left = node
-            node.parent = self
-        elif self.right == None:
-            self.right = node
-            node.parent = self
-        else:
-            return 0
-
-    def get_label(self):
-        return self.label
-
-    def set_node_sequence(self, sequence):
-        self.sequence = sequence
-
-    def get_character_in_sequence(self, i):
-        return self.sequence[i]
-
-    def set_node_score(self, score, i):
-        self.scores[i] = score
-
-    def get_node_alphabet_scores(self):
-        return self.scores
-
-    def set_node_alphabet_scores(self, scores):
-        self.scores = scores
-
-    def get_alphabet_index_from_score(self, value):
-        return self.scores.index(value)
-
-    def get_right_child(self):
-        return self.right
-
-    def get_left_child(self):
-        return self.left
-
-    def get_parent(self):
-        return self.parent
-
-    def set_parent(self, node):
-        self.parent = node
-
-# La méthode build_sequence_dict construit un dictionnaire avec les séquences présentées dans un fichier fasta nommé seqfile
-# La forme du dictionnaire:
-# {
-#    'seq1_label' : 'AAACGACGCAGCATCAT',
-#    ...                               ,
-#    'seqn_label' : 'ACATCATCA-ATCATCA'
-# }
 def build_sequence_dict(seqFile):   
     sequence, label = '',''
     sequenceDict = {}
@@ -108,7 +45,6 @@ def parse_newick_string(s, seqDict):
                 nodeID+=1
                 newNode = Node(leafLabel.strip())
                 newNode.set_node_sequence(seqDict[leafLabel.strip()])
-                print newNode.get_label()
                 currentNode.add_child(newNode)
                 leafLabel = ''
             if currentNode.get_right_child() is not None and currentNode.get_left_child() is not None:
@@ -122,7 +58,7 @@ def parse_newick_string(s, seqDict):
                 if currentNode is 0:
                     return 0
                 leafLabel = ''
-            if currentNode.get_right_child() is not None and currentNode.get_left_child() is not None:
+            if currentNode.get_right_child()    is not None and currentNode.get_left_child() is not None:
                 currentNode = currentNode.get_parent()
         else:
             leafLabel+=character
